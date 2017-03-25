441d7a107d7ee7656a23b82630e58cf7965d7e90
ktabak97 <K.Tabak@stud.elka.pw.edu.pl>
2017-03-26 00:08:08 +0100

    synchro

--- PROE_wypozyczalnia_nart_1/testwypozyczalnia.cpp
+++ PROE_wypozyczalnia_nart_1/testwypozyczalnia.cpp
@@ -11,32 +11,33 @@ void starttestwypozyczalnia()
 	cout << "Uruchamiam test sprzetu" << endl;
 	cout << "////////////////////////" << endl << endl;
 
-
 	Wypozyczalnia pierwsza("mysliborska", 3);
-
+	pierwsza.wypiszWszystko();
 	pierwsza.dodajNarty("elan");
 	pierwsza.dodajNarty("dynastar");
 	pierwsza.dodajNarty("volkl", 39, 158, Poziom_n::Zaawansowany, Dostepnosc_n::Dostepne);
 	pierwsza.dodajSnowboard("burton");
 	pierwsza.dodajKask("uvex");
-	pierwsza.dodajSnowboard("K2");
-	pierwsza.dodajKask("atomic");
+	pierwsza.dodajSnowboard("K2", 47, 145, Poziom_s::Ekspert, Dostepnosc_s::Wypozyczony);
+	pierwsza.dodajKask("atomic", 17, 52, Kolor_k::Czerwony);
 	
 	pierwsza.wypiszWszystko();
 
 	pierwsza.usunNarte("dynastar");
-	pierwsza.usunKask("atomic");
+	pierwsza.usunKask("uvex");
 	pierwsza.usunSnowboard("brton");
 
 	pierwsza.wypiszNarty();
 
+	Wypozyczalnia kopia(pierwsza);
+	kopia.dodajNarty("atomic");
+	kopia.wypiszWszystko();
 
+	kopia + "rossignol";
+	kopia.wypiszNarty();
 
 	cout << "////////////////////////" << endl;
 	cout << "Zakonczono test wypozyczalni" << endl;
 	cout << "////////////////////////" << endl << endl;
-	cout << "Utworzono w sumie: " << Wypozyczalnia::zwrocIloscWypozyczalni() << " obiekty/ow typu Wypozyczalnia" << endl;
-	cout << "Utworzono w sumie: " << Wypozyczalnia::zwrocIloscNart() << " obiekty/ow typu Narty" << endl;
-	cout << "Utworzono w sumie: " << Wypozyczalnia::zwrocIloscSnowboardow() << " obiekty/ow typu Snowboard" << endl;
-	cout << "Utworzono w sumie: " << Wypozyczalnia::zwrocIloscKaskow() << " obiekty/ow typu Kask" << endl;
-};
+	cout << "Utworzono w sumie: " << Wypozyczalnia::zwrocIloscWypozyczalni() << " obiekty/ow typu Wypozyczalnia" << endl << endl;
+	};
